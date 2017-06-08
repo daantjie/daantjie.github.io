a822c21109981dc5a922c8227830b42cbae6f9e2
Daniel Oosthuizen <danieltheexperimenter@gmail.com>
2017-06-08 19:49:10 +0100

    WIP POST: A condemnation of blind endorsement
    The trouble is, I think it might be slightly inconsistent! (Since I
    don't give much evidence, and actually make an appeal to authority by
    invoking Aristotle.)
    
    This flip-flopping in my decision process gives me the idea of creating
    many ‘characters’, or disagreeing voices -- that would certainly
    counterbalance the rhetoric, since all sides would be using it to some
    extent; leaving just the logos behind.

---
+++ blind-endorsement.poly.pm
@@ -0,0 +1,30 @@
+#lang pollen
+
+◊title{A condemnation of blind endorsement}
+
+◊quotation[#:person "Doug Hoyte" #:source "Let Over Lambda"]{
+◊sc{Common Lisp} is … by far the best programming environment available to
+current-day programmers.}
+
+Saying something over and over again doesn’t make it any more true.
+
+It’s not helpful, when you want to show people the interesting parts of Lisp,
+to call it the ◊scare{ultimate language}. Like anything, Lisp has upsides and
+downsides. If you’re someone who likes Lisp, (I hope) it’s not because people
+have ◊emph{told} you it’s great – rather, it’s because you’ve spent time with
+it, you’ve created things with it, and you’ve decided that its pros outweigh
+its cons.◊aside{Or that ◊function{cons} is its best pro!}
+
+This is perhaps a more general point about making good arguments: that you
+should give people the facts so they come to their own conclusion. Stuffing
+your prose with zealous rhetoric doesn’t make the facts more factual – it just
+hides the real, honest truth from the reader.
+
+This method of persuasion is called ◊first-use{logos}. It’s one of
+◊person{Aristotle}’s ◊first-use{three modes of persuasion}◊aside{
+  Though it turns out that there is a fourth, ◊first-use{kairos}, which is where
+  the orator uses the time and the setting to their advantage – however, this
+  isn’t key to ◊emph{written} arguments.
+}, and Aristotle describes it not just as the logic behind an argument, but also
+as giving a satisfaction to the listeners, who are given the freedom to make
+their own conclusions.
